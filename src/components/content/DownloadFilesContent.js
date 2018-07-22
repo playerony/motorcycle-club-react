e339d25792013b94fc78439213b41cf61fb13b76
playerony <pawel.wojtasinski.1995@gmail.com>
2018-07-22 16:02:27 +0200

    Update file icons

--- src/components/content/DownloadFilesContent.js
+++ src/components/content/DownloadFilesContent.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import attachment1 from '../../static/docx/Załącznik1.docx';
 import attachment2 from '../../static/docx/Załącznik2.docx';
-import fileDownload from 'react-file-download';
-import fetch from 'cross-fetch';
+import docxIcon from '../../static/img/docx.png'
 
 const StyledContent = styled.div`
     background-color: #eee;
     width: 100%;
-    height: 600px;
+    height: 400px;
 
     & > .container {
         margin: auto;
@@ -19,41 +18,64 @@ const StyledContent = styled.div`
 
         & > .container--file {
             width: 250px;
-            height: 250px;
-            background-color: yellow;
+            height: 300px;
+            background-color: #fff;
             margin: auto;
+            border-top: 4px solid #40a4c4;
+            box-shadow: 0px 3px 6px rgba(0, 0, 0, .3);
+            transition: 0.4s all ease-in-out;
+
+            &:hover {
+                border-top: 4px solid #515151;
+                box-shadow: inset 0px 300px 0px rgba(81, 81, 81, .1),
+                            0px 3px 6px rgba(0, 0, 0, .3);
+            }
+
+            & > .container--file--image {
+                background: url(${docxIcon});
+                width: 250px;
+                height: 250px;
+                background-size: cover;
+                background-position: center;
+                overflow: hidden;
+                outline: none;
+                cursor: pointer;
+            }
+
+            & > .container--file--text {
+                height: 50px;
+                line-height: 50px;
+                width: 100%;
+                text-align: center;
+                color: #515151;
+                display:inline-block;
+                text-decoration: none;
+                font-weight: 700;
+            }
         }
 
         @media screen and (max-width: 800px) {
             flex-direction: column;
         }
     }
+    
+    @media screen and (max-width: 800px) {
+        height: 800px;
+    }
 `;
 
 class DownloadFilesContent extends Component {
-    handleDownloadFile = () => {
-        fetch('../../static/docx/Załącznik1.docx')
-            .then((response) => {
-                console.log(response)
-
-                fetch(response.url)
-                    .then((response) => {
-                        console.log(response)
-
-                        fileDownload(response.data, "Załącznik1.docx");
-                    });
-            });
-    }
-
     render() {
         return (
             <StyledContent>
                 <div className="container">
                     <a href={attachment1} download="Załącznik1.docx" className="container--file">
-                        Załącznik1
+                        <div className="container--file--image"></div>
+                        <div className="container--file--text">Załącznik1.docx</div>
                     </a>
                     <a href={attachment2} download="Załącznik2.docx" className="container--file">
-                        Załącznik2
+                    <div className="container--file--image"></div>
+                        <div className="container--file--text">Załącznik2.docx</div>
                     </a>
                 </div>
             </StyledContent>
