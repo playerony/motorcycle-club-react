2f14b5be81c5d618e40ef43f09b473ac04c237f7
playerony <pawel.wojtasinski.1995@gmail.com>
2018-07-27 20:54:13 +0200

    Add animations

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -4,6 +4,7 @@ import img2 from '../../static/img/content2.png';
 import img3 from '../../static/img/content3.png';
 import img4 from '../../static/img/content4.png';
 import { slideInLeft } from 'react-animations';
+import Waypoint from 'react-waypoint';
 import './Content.scss';
 import { keyframes } from 'styled-components';
 
@@ -69,8 +70,40 @@ class Content extends Component {
                     }
                 ]
             }
-        ]
+        ],
+        content1Animation: false,
+        content2Animation: false,
+        content3Animation: false,
+        content4Animation: false
     };
+
+    handleContent1Animation = () => {
+        this.setState({
+            ...this.state,
+            content1Animation: true
+        })
+    }
+
+    handleContent2Animation = () => {
+        this.setState({
+            ...this.state,
+            content2Animation: true
+        })
+    }
+
+    handleContent3Animation = () => {
+        this.setState({
+            ...this.state,
+            content3Animation: true
+        })
+    }
+
+    handleContent4Animation = () => {
+        this.setState({
+            ...this.state,
+            content4Animation: true
+        })
+    }
 
     render() {
         const slideInLeftAnimation = keyframes`${slideInLeft}`;
@@ -80,50 +113,58 @@ class Content extends Component {
                 <div id="content">
                     <h1><span>Dlaczego</span> WKM?</h1>
                     <div className="boxes">
-                        <div className="boxes--box" id="content1">
-                            <div className="boxes--box--image" style={{animation: `2s ${slideInLeftAnimation}`}}>
-                                <img src={this.state.boxes[0].image} alt={this.state.boxes[0].image}/>
-                            </div>
-                            <div className="boxes--box--text" style={{transform: `translateY(-50%)`}}>
-                                <div className="boxes--box--text-title">
-                                    {this.state.boxes[0].title}
+                        <Waypoint onEnter={this.handleContent1Animation}>
+                            <div className="boxes--box" id="content1">
+                                <div className="boxes--box--image" style={{animation: this.state.content1Animation ? `3s ${slideInLeftAnimation}` : ``}}>
+                                    <img src={this.state.boxes[0].image} alt={this.state.boxes[0].image}/>
                                 </div>
-                                <h3>{this.state.boxes[0].contents[0].content}</h3>
-                            </div>
-                        </div>
-                        <div className="boxes--box">
-                            <div className="boxes--box--text" style={{transform: `translateY(-50%)`}}>
-                                <div className="boxes--box--text-title">
-                                    {this.state.boxes[1].title}
+                                <div className="boxes--box--text" style={{animation: this.state.content1Animation ? `2s ${slideInLeftAnimation}` : ``}}>
+                                    <div className="boxes--box--text-title">
+                                        {this.state.boxes[0].title}
+                                    </div>
+                                    <h3>{this.state.boxes[0].contents[0].content}</h3>
                                 </div>
-                                <h3>{this.state.boxes[1].contents[0].content}</h3>
                             </div>
-                            <div className="boxes--box--image">
-                                <img src={this.state.boxes[1].image} alt={this.state.boxes[1].image}/>
-                            </div>
-                        </div>
-                        <div className="boxes--box">
-                            <div className="boxes--box--image">
-                                <img src={this.state.boxes[2].image} alt={this.state.boxes[2].image}/>
-                            </div>
-                            <div className="boxes--box--text" style={{transform: `translateY(-65%)`}}>
-                                <div className="boxes--box--text-title">
-                                    {this.state.boxes[2].title}
+                        </Waypoint>
+                        <Waypoint onEnter={this.handleContent2Animation}>
+                            <div className="boxes--box" id="content2">
+                                <div className="boxes--box--text" style={{animation: this.state.content2Animation ? `2s ${slideInLeftAnimation}` : ``}}>
+                                    <div className="boxes--box--text-title">
+                                        {this.state.boxes[1].title}
+                                    </div>
+                                    <h3>{this.state.boxes[1].contents[0].content}</h3>
                                 </div>
-                                <h3>{this.state.boxes[2].contents[0].content}</h3>
-                            </div>
-                        </div>
-                        <div className="boxes--box">
-                            <div className="boxes--box--text" style={{transform: `translateY(-60%)`}}>
-                                <div className="boxes--box--text-title">
-                                    {this.state.boxes[3].title}
+                                <div className="boxes--box--image" style={{animation: this.state.content2Animation ? `2s ${slideInLeftAnimation}` : ``}}>
+                                    <img src={this.state.boxes[1].image} alt={this.state.boxes[1].image}/>
                                 </div>
-                                <h3>{this.state.boxes[3].contents[0].content}</h3>
                             </div>
-                            <div className="boxes--box--image">
-                                <img src={this.state.boxes[3].image} alt={this.state.boxes[3].image}/>
+                        </Waypoint>
+                        <Waypoint onEnter={this.handleContent3Animation}>
+                            <div className="boxes--box">
+                                <div className="boxes--box--image" style={{animation: this.state.content3Animation ? `2s ${slideInLeftAnimation}` : ``}}>
+                                    <img src={this.state.boxes[2].image} alt={this.state.boxes[2].image}/>
+                                </div>
+                                <div className="boxes--box--text" style={{animation: this.state.content3Animation ? `2s ${slideInLeftAnimation}` : ``}}>
+                                    <div className="boxes--box--text-title">
+                                        {this.state.boxes[2].title}
+                                    </div>
+                                    <h3>{this.state.boxes[2].contents[0].content}</h3>
+                                </div>
                             </div>
-                        </div>
+                        </Waypoint>
+                        <Waypoint onEnter={this.handleContent4Animation}>
+                            <div className="boxes--box">
+                                <div className="boxes--box--text" style={{animation: this.state.content4Animation ? `2s ${slideInLeftAnimation}` : ``}}>
+                                    <div className="boxes--box--text-title">
+                                        {this.state.boxes[3].title}
+                                    </div>
+                                    <h3>{this.state.boxes[3].contents[0].content}</h3>
+                                </div>
+                                <div className="boxes--box--image" style={{animation: this.state.content4Animation ? `2s ${slideInLeftAnimation}` : ``}}>
+                                    <img src={this.state.boxes[3].image} alt={this.state.boxes[3].image}/>
+                                </div>
+                            </div>
+                        </Waypoint>
                     </div>
                     <div className="content-description">
                         <h1>{this.state.farewell}</h1>
